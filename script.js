c54972c66775d707c7e3fdb5763c6dd8ea76a3f7
Vincent <vincent854321@gmail.com>
2023-06-01 20:19:46 -0500

    added delete and change buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 let a = new Book("Things We Never Got Over", "Lucy Score", 102, true)
 let b = new Book("The Power of Now: A Guide to Spiritual Enlightenment", "Eckhart Tolle", 236, false);
-let c = new Book("Power vs. Force","David R. Hawkins", 341, false);
+let c = new Book("Power vs. Force", "David R. Hawkins", 341, false);
 addBookToLibrary(a);
 addBookToLibrary(b);
 addBookToLibrary(c);
@@ -13,10 +13,10 @@ button.addEventListener("click", function () {
     form.classList.toggle("hidden");
 })
 
+
 let form = document.querySelector("#form-div");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("hi")   
     const title = document.getElementById("title");
     const author = document.getElementById("author");
     const pages = document.getElementById("pages");
@@ -24,6 +24,9 @@ form.addEventListener("submit", function (event) {
     let book = new Book(title.value, author.value, pages.value, read.checked);
     addBookToLibrary(book);
     displayLibrary();
+    form.reset();
+    let divform = document.querySelector(".form");
+    divform.classList.toggle("hidden");
 })
 
 function Book(title, author, pages, read) {
@@ -33,11 +36,13 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+
+
 function clearLibrary() {
     let bookDiv = document.querySelector(".library");
-    while (bookDiv.hasChildNodes()){
+    while (bookDiv.hasChildNodes()) {
         bookDiv.removeChild(bookDiv.firstChild);
-    } 
+    }
 }
 
 function displayLibrary() {
@@ -46,7 +51,17 @@ function displayLibrary() {
     myLibrary.forEach(book => {
         let newBook = document.createElement("div");
         newBook.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}, Read: ${book.read}`;
+
+
+        let deletebutton = document.createElement("button");
+        deletebutton.innerText = "Delete Entry";
+        deletebutton.classList.add("delete");
+
+        let readbutton = document.createElement("button");
+        readbutton.innerText = "read";
         bookDiv.appendChild(newBook);
+        bookDiv.appendChild(deletebutton);
+        bookDiv.appendChild(readbutton);
     });
 }
 
